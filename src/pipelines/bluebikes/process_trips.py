2c76f663256e0b7b58f725e4724946746f18038b
messiahr <138614440+messiahr@users.noreply.github.com>
2026-06-24 09:56:37 -0400

    refactor(cleaning): use LazyFrames in processing pipelines, reducing time from ~90 seconds to ~30 seconds

--- src/pipelines/bluebikes/process_trips.py
+++ src/pipelines/bluebikes/process_trips.py
@@ -1,7 +1,4 @@
-from pathlib import Path
-
 import polars as pl
-from tqdm import tqdm
 
 from config import PROCESSED_DIR, RAW_DIR
 from pipelines.bluebikes.column_mapping import (
@@ -12,25 +9,22 @@ from pipelines.bluebikes.column_mapping import (
 )
 
 
-def process_file(path: Path) -> pl.DataFrame:
-    # \\N is an artifact from MySQL
-    # TODO: extract line below to repository layer
-    df = pl.read_csv(path, infer_schema_length=False, null_values=["\\N"])
-    df = standardize_columns(df)
-    df = clean_values(df)
-    df = select_columns(df)
-    return df
+def process_trips(lf: pl.LazyFrame) -> pl.LazyFrame:
+    return lf.pipe(standardize_columns).pipe(clean_values).pipe(select_columns)
 
 
-def standardize_columns(df: pl.DataFrame) -> pl.DataFrame:
-    return df.rename(COLUMN_MAPPING, strict=False)
+def standardize_columns(lf: pl.LazyFrame) -> pl.LazyFrame:
+    return lf.rename(COLUMN_MAPPING, strict=False)
 
 
-def clean_values(df: pl.DataFrame) -> pl.DataFrame:
+def clean_values(lf: pl.LazyFrame) -> pl.LazyFrame:
+    # Get columns
+    columns = lf.collect_schema().names()
+
     # Parse datetimes
     for time in ["started_at", "ended_at"]:
-        if time in df.columns:
-            df = df.with_columns(
+        if time in columns:
+            lf = lf.with_columns(
                 pl.col(time)
                 .str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S%.f", strict=False)
                 .dt.truncate("1s")
@@ -38,40 +32,42 @@ def clean_values(df: pl.DataFrame) -> pl.DataFrame:
             )
 
     # Standardize names for (un)subscribed customers
-    if "member_casual" in df.columns:
-        df = df.with_columns(pl.col("member_casual").replace(CUSTOMER_MAP))
+    if "member_casual" in columns:
+        lf = lf.with_columns(pl.col("member_casual").replace(CUSTOMER_MAP))
 
     # Parse birth_year as Int32 (nullable)
-    if "birth_year" in df.columns:
-        df = df.with_columns(pl.col("birth_year").cast(pl.Int32, strict=False))
+    if "birth_year" in columns:
+        lf = lf.with_columns(pl.col("birth_year").cast(pl.Int32, strict=False))
 
     # Convert gender to String
-    if "gender" in df.columns:
-        df = df.with_columns(pl.col("gender").replace(GENDER_MAP))
+    if "gender" in columns:
+        lf = lf.with_columns(pl.col("gender").replace(GENDER_MAP))
 
     # Parse ZIP code as Int32 (nullable)
-    if "postal_code" in df.columns:
-        df = df.with_columns(pl.col("postal_code").cast(pl.Int32, strict=False))
+    if "postal_code" in columns:
+        lf = lf.with_columns(pl.col("postal_code").cast(pl.Int32, strict=False))
 
     # Remove rows missing critical fields
     critical = ["start_station_id", "end_station_id", "started_at", "ended_at"]
     for col in critical:
-        if col in df.columns:
-            df.drop_nulls(subset=[col])
+        if col in columns:
+            lf.drop_nulls(subset=[col])
 
-    return df
-
-
-def select_columns(df: pl.DataFrame) -> pl.DataFrame:
-    return df.select([c for c in FINAL_COLUMNS if c in df.columns])
+    return lf
 
 
-# TODO: replace with PostgreSQL
+def select_columns(lf: pl.LazyFrame) -> pl.LazyFrame:
+    return lf.select([c for c in FINAL_COLUMNS if c in lf.collect_schema().names()])
+
+
+# TODO: replace with repository model
 if __name__ == "__main__":
-    csv_files = sorted([p for p in RAW_DIR.glob("*csv") if not p.name.startswith(".")])
-    df = pl.DataFrame()
-    for path in tqdm(
-        csv_files, desc="󱄟 Processing", bar_format="{desc}: |{bar}| {percentage:.1f}%"
-    ):
-        df = pl.concat([df, process_file(path)], how="diagonal_relaxed")
-    df.write_csv(PROCESSED_DIR / "trips.csv")
+    lfs = []
+
+    for path in sorted([p for p in RAW_DIR.glob("*csv") if not p.name.startswith(".")]):
+        lf = pl.scan_csv(path, infer_schema_length=False, null_values=["\\N"])
+        lfs.append(process_trips(lf))
+
+    lf = pl.concat(lfs, how="diagonal_relaxed")
+
+    lf.sink_parquet(PROCESSED_DIR / "trips.parquet")
