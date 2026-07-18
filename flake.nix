50117d0bbd4427af5449854dca8c62af58d563cf
messiahr <ridgleymessiah@gmail.com>
2026-07-18 14:01:21 -0400

    tooling: fix flake.nix to allow C-compiled Python packages

--- flake.nix
+++ flake.nix
@@ -26,9 +26,27 @@
             ruff
             pre-commit
             pyright
+            # Core libraries for compiled Python packages
+            stdenv
+            gfortran
+            libz
+            glib
+            openblas
+            gcc
+            pkg-config
+            libffi
+            openssl
           ];
 
           shellHook = ''
+            export CC=${pkgs.gcc}/bin/gcc
+            export CXX=${pkgs.gcc}/bin/g++
+            export FC=${pkgs.gfortran}/bin/gfortran
+            export NPY_NUM_BUILD_JOBS=$(nproc)
+            export CFLAGS="-I${pkgs.openblas}/include -I${pkgs.libffi}/include"
+            export LDFLAGS="-L${pkgs.openblas}/lib -L${pkgs.libffi}/lib"
+            export PKG_CONFIG_PATH="${pkgs.openblas}/lib/pkgconfig:${pkgs.openssl}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libz}/lib:${pkgs.glib}/lib:$LD_LIBRARY_PATH"
             echo "Python $(python --version)"
             echo "uv $(uv --version)"
             echo "Development environment ready. Run 'make setup' to install dependencies."
