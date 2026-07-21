57b0665a131d73323a2bfa5ddd01a07336a93e7f
messiahr <ridgleymessiah@gmail.com>
2026-07-21 14:18:46 -0400

    feature(processed_data_repository.py): add repository for persisting processed data in S3

---
+++ src/adapters/processed_data_repository.py
@@ -0,0 +1,26 @@
+import boto3
+import polars as pl
+
+from config import AWS_BUCKET
+
+
+class ProcessedDataRepository:
+    def __init__(self) -> None:
+        self._bucket = AWS_BUCKET
+        self._key = "all_trips_standardized.parquet"
+        self._s3 = boto3.client("s3")
+        self.data_path = f"s3://{self._bucket}/{self._key}"
+
+    def save(self, df: pl.LazyFrame) -> str:
+        df.sink_parquet(self.data_path)
+        return self.data_path
+
+    def load(self) -> pl.LazyFrame:
+        return pl.scan_parquet(self.data_path)
+
+    def exists(self) -> bool:
+        try:
+            self._s3.head_object(Bucket=self._bucket, Key=self._key)
+            return True
+        except self._s3.exceptions.ClientError:
+            return False
